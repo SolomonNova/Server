5a3ed993fd2fad28f36e329cf8f1f1f6da8bdd9a
AKASH KUMAR <993284vks@gmail.com>
2025-12-14 19:48:21 +0530

    added some http parsing features // Not completed yet.

---
+++ ServerLinux/HTTP_PARSER.h
@@ -0,0 +1,48 @@
+#ifndef HTTP_PARSER_H
+#define HTTP_PARSER_H
+
+typedef struct Headers
+{
+    char* keys[64];
+    char* values[1024];
+} Headers;
+
+typedef struct Request
+{
+    char method[8];
+    char path[1024];
+    char version[8];
+    Headers* headers;
+    char* body;
+} Request;
+
+char** parseMethod(const char* request);
+Headers* parseHeaders(const char* request);
+char* parseBody(const char* s, Request* request);
+char* parseChunckedBody(char* s);
+
+//============Helper Functions================//
+
+char** tokenizer(const char* s);
+/*
+    Takes a string and returns an array of pointers
+    to its substrings that were seperated by a space character ' '.
+*/
+
+char** getLineKeyValue(const char* s, int i, int j);
+/*
+    Takes a string with the last 2 characters as '\r' and '\n' and
+    a single colon ':' anywhere between i and j index.
+    Note: i and j are 0th and second last characters when considiring
+    the null terminiator as a part of the string.
+*/
+
+unsigned short hashFunction(const char* s);
+
+char* getValue(const char* s, Headers* headers);
+/*
+    Gets the value corrosponding the key string s
+*/
+
+
+#endif
--- ServerLinux/HTTP_Parser.c
+++ ServerLinux/HTTP_Parser.c
@@ -1,11 +1,12 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
-#include "HTTP_Parser.h"
+#include "HTTP_PARSER.h"
 
 /////////////////////////////////////////
 /////////////////////////////////////////
-char** tokenizer(char* s)
+char** tokenizer(const char* s)
 {
     int len = strlen(s);
     int tokenCount = 1;
@@ -47,7 +48,7 @@ char** tokenizer(char* s)
 
 /////////////////////////////////////////
 /////////////////////////////////////////
-char** getLineKeyValue(char* s, int i, i)
+char** getLineKeyValue(const char* s, int i, int j)
 {
     int subSize = j - i + 1;
 
@@ -76,7 +77,7 @@ char** getLineKeyValue(char* s, int i, i)
 
 /////////////////////////////////////////
 /////////////////////////////////////////
-char** parseMethod(char* request)
+char** parseMethod(const char* request)
 {
     unsigned int len = strlen(request);
     unsigned int endIndex = 0;
@@ -106,24 +107,168 @@ char** parseMethod(char* request)
 
 /////////////////////////////////////////
 /////////////////////////////////////////
-Headers* parseHeaders(char* request)
+Headers* parseHeaders(const char* request)
 {
-    // get the starting and ending index of the headers
-
     unsigned int len = strlen(request);
+    int startIndex = -1;
+    int endIndex = -1;
 
+    for (int x = 0; x + 1 < len; x++)
+        if (request[x] == '\r' && request[x + 1] == '\n')
+        {
+            startIndex = x + 2;
+            break;
+        }
+
+    if (startIndex == -1) return NULL;
+
+    for (int x = startIndex; x + 3 < len; x++)
+        if (request[x] == '\r' && request[x + 1] == '\n' &&
+            request[x + 2] == '\r' && request[x + 3] == '\n')
+        {
+            endIndex = x - 1;
+            break;
+        }
+
+    if (endIndex == -1) return NULL;
+
+    int i = startIndex;
+    int j = startIndex;
+    int count = 0;
+
+    char*** keysCollection = (char***)calloc(64, sizeof(char**));
+
+    while (j <= endIndex)
+    {
+        if (request[j] == '\r')
+        {
+            keysCollection[count] = getLineKeyValue(request, i, j - 1);
+            j += 2;
+            i = j;
+            count++;
+            continue;
+        }
+        j++;
+    }
+
+    Headers* headers = (Headers*)calloc(1, sizeof(Headers));
+
+    for (int x = 0; x < count; x++)
+    {
+        char* key = keysCollection[x][0];
+        char* value = keysCollection[x][1];
+
+        headers->keys[x] = key;
+
+        unsigned short index = hashFunction(key);
+        while (headers->values[index] != NULL)
+            index = (index + 1) % 1024;
+
+        headers->values[index] = value;
+    }
+
+    headers->keys[count] = NULL;
+
+    free(keysCollection);
+
+    return headers;
+}
+
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+char* getValue(const char* s, Headers* headers)
+{
+
+    unsigned short index = hashFunction(s);
+
+    while (headers->values[index] != NULL)
+    {
+        if (strcmp(headers->keys[index], s) == 0)
+            return headers->values[index];
+
+        index = (index + 1) % 1024;
+    }
+
+    return NULL;
+}
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+unsigned short hashFunction(const char* s)
+{
+    unsigned int result = 0;
+
+    const char* p = s;
+
+    while (p)
+    {
+        result += *p;
+        p++;
+    }
+
+    return (unsigned short)((result % 1024) / 2);
+}
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+char* parseBody(const char* s, Request* request)
+{
     int startIndex = 0;
-    int endIndex = 0;
+    int len = (int)strlen(s);
+
+    // find the index of the first character of Body
 
     for (int x = 0; x < len; x++)
     {
-        if (request[x] == '\n') startIndex = x + 1;
 
-        if (request[x] == '\n' && request[x + 1] == '\r' && request[x + 2] == '\n') endIndex = x - 1;
+        if (s[x] == '\r' && s[x + 1] == '\n' &&
+            s[x + 2] == '\r' && s[x + 3] == '\n')
+        {
+            {
+                startIndex = x + 4;
+            }
+        }
+
     }
 
-    int i = 0;
-    int j = 0;
+    if (request->headers->values[hashFunction("Content-Length")] > 0)
+    {
+        int bodyLength = 0;
+        char* lenString = request->headers->values[hashFunction("Content-Length")];
 
-    // to be finished
+        for (int x = 0; x < '\r'; x++)
+        {
+            if (x > '9' || x < '0') continue;
+
+            bodyLength = bodyLength * 10 + lenString[x - '0'];
+        }
+
+        char buffer[bodyLength + 1];
+
+        for (int x = startIndex; x < len; x++)
+        {
+            buffer[startIndex - x] = s[x];
+        }
+
+        return buffer;
+    }
+
+    else
+    {
+
+    }
 }
+
+char* parseChunckedBody(char* s, int start, int end, int isLenAvailabe)
+{
+    if (isLenAvailabe == 0)
+    {
+        // find the start and end index of the hex size
+
+    }
+}
+
+
+
+
